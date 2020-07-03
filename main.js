dfa44a371c60fe93c8c0e559236a85c3c1fb4c96
Deven Gurung <deven9229@gmail.com>
2020-07-03 14:31:24 +0600

    added images and refactored code for html and css

--- main.js
+++ main.js
@@ -2,57 +2,72 @@
 let scoreboard = {
     player: 0,
     computer: 0
-}
+};
+
+const rock = document.querySelector("#rock");
+rock.addEventListener('click', () => {
+playRound("rock");
+});
+
+const paper = document.querySelector("#paper");
+rock.addEventListener('click', () => {
+    playRound("paper");
+});
+
+const scissors = document.querySelector("#scissors");
+rock.addEventListener('click', () => {
+    playRound("scissors");
+});
 
 //Computer choice which will select randomly 
 function computerPlay() {
- const choices = ['rock', 'paper', 'scissors']
- const randomChoice = Math.floor(Math.random() * 3)
- return choices[randomChoice]
+ const choices = ['rock', 'paper', 'scissors'];
+ const randomChoice = Math.floor(Math.random() * 3);
+ return choices[randomChoice];
 }
 
 //Single round choice 
 function playRound(playerSelection, computerSelection) {
     //Checking for a tie
     if (playerSelection === computerSelection) {
-        return 'It is a tie'
+        return 'It is a tie';
     }
     //Checking for rock
     if (playerSelection === 'rock') {
         if (computerSelection === 'scissors') {
-            scoreboard.player++
-            return 'You win! Rock beats scissors'
+            scoreboard.player++;
+            return 'You win! Rock beats scissors';
         } else {
-            scoreboard.computer++
-            return 'You Lose! Paper beats rock'
+            scoreboard.computer++;
+            return 'You Lose! Paper beats rock';
         }
     }
     //Checking for paper
     if (playerSelection === 'paper') {
         if (computerSelection === 'scissors') {
-            scoreboard.player++
-            return 'You Lose! Scissors beats paper'
+            scoreboard.player++;
+            return 'You Lose! Scissors beats paper';
         } else {
-            scoreboard.computer++
-            return 'You Win! Rock beats scissors'
+            scoreboard.computer++;
+            return 'You Win! Rock beats scissors';
         }
     }
     //Checking for rock
     if (playerSelection === 'scissors') {
         if (computerSelection === 'rock') {
-            scoreboard.player++
-            return 'You Lose! Rock beats scissors'
+            scoreboard.player++;
+            return 'You Lose! Rock beats scissors';
         } else {
-            scoreboard.computer++
-            return 'You Win! Scissors beats paper'
+            scoreboard.computer++;
+            return 'You Win! Scissors beats paper';
         }
     }
 }
 //Game function to play 5 rounds
 function game() {
-    const playerSelection = prompt('Pick your weapon')
-    const computerSelection = computerPlay()
-    console.log(playRound(playerSelection, computerSelection))
+
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
 }
 
 for (let round = 0; round < 5; round++) {
@@ -61,14 +76,14 @@ for (let round = 0; round < 5; round++) {
 
 //Result the final scores
 function result() {
-    console.log(`Computer Score: ${scoreboard.computer} Player Score: ${scoreboard.player}`)
+    console.log(`Computer Score: ${scoreboard.computer} Player Score: ${scoreboard.player}`);
     if (scoreboard.computer > scoreboard.player) {
-        console.log('The computer wins!')
+        console.log('The computer wins!');
     }else if (scoreboard.computer < scoreboard.player) {
-        console.log('The player wins!')
+        console.log('The player wins!');
     }else{
-        console.log('It is a draw')
+        console.log('It is a draw');
     }
 }
 
-result()
+result();
